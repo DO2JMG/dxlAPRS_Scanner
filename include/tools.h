0aae7269be7b399639cce0fdec9bbc362e95b960
Jean-Michael <34538443+DO2JMG@users.noreply.github.com>
2023-10-14 08:28:08 +0200

    Add files via upload

--- include/tools.h
+++ include/tools.h
@@ -1,17 +1,17 @@
 #ifndef	tools_H
 #define	tools_H
 
+#pragma once
+
 using namespace std;
 
-class tools
-{
-        public:
-  
+namespace tools {
         void debug(std::string strData, bool newline);
         void file_write(const string strFilename, string strData);
         void file_write_append(const string strFilename, string strData);
         int gettimestamp();
         vector<std::string> splitString(const std::string& str);
+        int round_double(double value);
 };
 
 #endif
