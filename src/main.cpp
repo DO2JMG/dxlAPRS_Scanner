6d4171031a3c1fd04afd1f8d258deab1a889c2fc
Jean-Michael <34538443+DO2JMG@users.noreply.github.com>
2023-10-14 08:28:51 +0200

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -20,19 +20,20 @@
 #include "tools.h"
 
 using namespace std;
+using namespace tools;
 
 struct scanner_config {
   bool verbous = false;
   int port = 0; 
   int steps = 0;
   int startfrequency = 0;
-  int countermaxnoise = 100;
-  int counterminpeaks = 3;
-  int minbandwidth = 5;
-  int maxbandwidth = 22;
+  int cmn = 100;
+  int cmp = 3;
+  int minbw = 5;
+  int maxbw = 22;
   int squelch = 65; 
   int timer_peaks = 30; // refresh frequency list after 60 seconds
-  int timer_holding = 120; // frequencies clear after 120 seconds
+  int timer_holding = 180; // frequencies clear after 120 seconds
   int level = 5;
   string filename;
   string blacklist;
@@ -46,27 +47,27 @@ struct frequency_list {
 };
 
 scanner_config config;
-tools tool;
 
 vector<int> peaks;
-vector<frequency_list> frequencies;
-vector<int> blacklist;
+vector<frequency_list> vfq;
+vector<int> vbl;
 
-double round_to(double value, double precision = 1.0) {
-    return round(value / precision) * precision;
+std::vector<int> peakDecimation(const std::vector<int>& input, int decimationFactor) {
+    std::vector<int> output;
+    for (size_t i = 0; i < input.size(); i += decimationFactor) {
+        int maxVal = input[i];
+        for (int j = 1; j < decimationFactor && i + j < input.size(); ++j) {
+            maxVal = std::max(maxVal, input[i + j]);
+        }
+        output.push_back(maxVal);
+    }
+    return output;
 }
 
-
-int round_double(double value) {
-    double result = round_to(value, 0.01);
-
-    return result;
-} 
-
-bool frequencyisonlist(double frequency) {
+bool frequencyisonlist(double fq) {
   if (peaks.size() > 0) {
     for (long unsigned i = 0; i < (peaks.size()); i++) {
-      if (peaks[i] == frequency) {
+      if (peaks[i] == fq) {
         return true;
       }
     }
@@ -80,7 +81,7 @@ int receivedata() {
   char buffer[1500] = {0};
 
   if ((serSockDes = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-    tool.debug("socket creation error...", false);
+    debug("socket creation error...", false);
     exit(-1);
   }
 
@@ -89,15 +90,15 @@ int receivedata() {
   serAddr.sin_addr.s_addr = INADDR_ANY;
 
   if ((bind(serSockDes, (struct sockaddr*)&serAddr, sizeof(serAddr))) < 0) {
-    tool.debug("binding error...", false);
+    debug("binding error...", false);
     close(serSockDes);
     exit(-1);
   }
 
   cliAddrLen = sizeof(cliAddr);
 
-  if (config.verbous) tool.debug("Start frequency " + to_string(config.startfrequency) + "hz", false);
-  if (config.verbous) tool.debug("Steps : " + to_string(config.steps) + "hz", true);
+  if (config.verbous) debug("Start frequency " + to_string(config.startfrequency) + "hz", false);
+  if (config.verbous) debug("Steps : " + to_string(config.steps) + "hz", true);
 
 
   while (true) {
@@ -105,7 +106,7 @@ int receivedata() {
 
 
     if (buffer[0] == 'S' && buffer[1] == 'D' && buffer[2] == 'R' && buffer[3] == 'L' && readStatus > 0) { // receive data from sdrtst
-      tool.debug("------------- Receiving data from port " + to_string(config.port), false);
+      debug("------------- Receiving data from port " + to_string(config.port), false);
 
       int dlen = sizeof(buffer);
 
@@ -119,10 +120,12 @@ int receivedata() {
           db.push_back(buffer[i]); 
         }
       }
-      
+
       // calculateing average
 
-      tool.debug("Calculateing average", false);
+      debug("DB Count : " + to_string(db.size()) , true);
+
+      debug("Calculateing average", false);
 
       int avg_all = 0;
       int avg_min = 1024;
@@ -135,12 +138,12 @@ int receivedata() {
         if (*i < avg_min) { avg_min = *i; }
         if (*i > avg_max) { avg_max = *i; }
 
-        if (dbc <= config.countermaxnoise) {
+        if (dbc <= config.cmn) {
           dbs += *i;
           dbc++;
         } else {
-          db_avg.push_back(dbs / config.countermaxnoise);
-          if (config.verbous) tool.debug("db noise block : " + to_string(dbs / config.countermaxnoise), false);
+          db_avg.push_back(dbs / config.cmn);
+          if (config.verbous) debug("db noise block : " + to_string(dbs / config.cmn), false);
 
           dbs = 0;
           dbc = 0;
@@ -151,42 +154,22 @@ int receivedata() {
 
       avg_all = (avg_all / db_avg.size());
 
-      if (config.verbous) tool.debug("db avg : " + to_string(avg_all), false);
-      if (config.verbous) tool.debug("db min : " + to_string(avg_min), false);
-      if (config.verbous) tool.debug("db max : " + to_string(avg_max), false);
+      if (config.verbous) debug("db avg : " + to_string(avg_all), false);
+      if (config.verbous) debug("db min : " + to_string(avg_min), false);
+      if (config.verbous) debug("db max : " + to_string(avg_max), false);
 
       // detecting peaks
-      tool.debug("Detecting peaks", false);
-
-      //-----------------------------------------------
-
-      float db_spike3 = 10.0;
-      int spike_wl3 = 3; 
-      int spike_wl5 = 5;
-
-      int dbsize = db.size();
-
-      for (long unsigned i = 0; i < (db.size()); i++) {
-        if ( db[i] - db[(i-spike_wl5+dbsize)%dbsize] > db_spike3
-            && db[i] - db[(i-spike_wl3+dbsize)%dbsize] > db_spike3
-            && db[i] - db[(i+spike_wl3+dbsize)%dbsize] > db_spike3
-            && db[i] - db[(i+spike_wl5+dbsize)%dbsize] > db_spike3
-            ) {
-              db[i] = (db[(i-spike_wl3+dbsize)%dbsize] +db[(i+spike_wl3+dbsize)%dbsize])/2.0;
-          }
-      }
-
-      //-----------------------------------------------
+      debug("Detecting peaks", false);
 
       for (long unsigned i = 0; i < (db.size()); i++) { // add peaks to the list
         if (db[i] > (avg_all + ((avg_all - avg_min)) - config.level)) {
-          int ifq = (i)*config.steps + (config.startfrequency);
+          int ifq = (i)*config.steps + (config.startfrequency + 1500);
          
           ifq = ifq/1000.0;  // hz in khz
         
           if (frequencyisonlist(ifq) == false) { // frequency is not on the list
             peaks.push_back(ifq);
-            if (config.verbous) tool.debug("detected peak at " + to_string(ifq) + "hz db = " + to_string(db[i]), false);
+            if (config.verbous) debug("detected peak at " + to_string(ifq) + "hz db = " + to_string(db[i]), false);
           }
         }
       } 
@@ -211,17 +194,17 @@ int getpeaks() {
 
     // Load blacklist
 
-    blacklist.clear(); // clear blacklist
+    vbl.clear(); // clear blacklist
 
-    fstream fblacklist;
+    fstream blf;
 
-    fblacklist.open(config.blacklist,ios::in);
-    if (fblacklist.is_open()) { 
+    blf.open(config.blacklist,ios::in);
+    if (blf.is_open()) { 
       string line;
-      while(getline(fblacklist, line)) {
+      while(getline(blf, line)) {
         if (line.length() > 1) {
-          blacklist.push_back(stoi(line));
-          if (config.verbous) tool.debug(line, false);
+          vbl.push_back(stoi(line));
+          if (config.verbous) debug(line, false);
         }
       }
     }
@@ -233,74 +216,79 @@ int getpeaks() {
     int i = 0;
 
     if (peaks.size() > 0) {
-      tool.debug("------------- peaks...", false);
+      debug("------------- peaks...", false);
 
       fqc = peaks.size();
 
       i = 0;
       pib = 0;   
-       while ((i < fqc) && (frequencies.size() < 400)) {
+       while ((i < fqc) && (vfq.size() < 400)) {
         fql = peaks[pib];
-        if (config.verbous) tool.debug("smallest frequency " + to_string((int)fql), false);
+        if (config.verbous) debug("smallest frequency " + to_string((int)fql), false);
 
         while((pib < fqc-1) && ((peaks[pib+1] - peaks[pib]) < 5.0)) { pib++; }
 
-        if (config.verbous) tool.debug("greatest frequency "+to_string(peaks[pib])+", at index "+to_string(pib),false);
+        if (config.verbous) debug("greatest frequency "+to_string(peaks[pib])+", at index "+to_string(pib),false);
 
-        if ((pib - i) >= config.counterminpeaks)  // 
+        if ((pib - i) >= config.cmp)  // 
         {
-          if (config.verbous) tool.debug("strong signals detected with more or equal " + to_string(config.counterminpeaks + 1) + " nearby frequency",false);
+          if (config.verbous) debug("strong signals detected with more or equal " + to_string(config.cmp + 1) + " nearby frequency",false);
 
-          frequency_list new_frequency;
+          frequency_list nfq;
           fqh = peaks[pib];
           fq = (fqh - fql);
-          new_frequency.bandwidth = fq;
-          if (new_frequency.bandwidth > config.maxbandwidth) {new_frequency.bandwidth = config.maxbandwidth; }
+          nfq.bandwidth = fq;
+          if (nfq.bandwidth > config.maxbw) {nfq.bandwidth = config.maxbw; }
 
           fq = fq / 2;
           fq = (fq > (floor(fq)+0.5f)) ? ceil(fq) : floor(fq);
 
           double rounded_frequency = round_double(((fql + fq)/1000) * 100.0) / 100.0; 
+          rounded_frequency = rounded_frequency *1000;
+
 
           bool ison = false;  
  
-          for (i = 0; i < frequencies.size(); i++) { 
-            if ((rounded_frequency*1000) == frequencies[i].frequency) { //check frequency is on the list
-              frequencies[i].timestamp = tool.gettimestamp();
+          for (i = 0; i < vfq.size(); i++) { 
+            if ((rounded_frequency) == vfq[i].frequency) { //check frequency is on the list
+              vfq[i].timestamp = gettimestamp();
+              ison = true;
+            }
+            if ((rounded_frequency) == (vfq[i].frequency-10) || (rounded_frequency) == (vfq[i].frequency+10)) {
               ison = true;
             }
           }
 
       
-          for (i = 0; i < blacklist.size(); i++) { 
-            int frequency_check = rounded_frequency*1000;
-            if (to_string(frequency_check) == to_string(blacklist[i])) { //check frequency is on the blacklist
+          for (i = 0; i < vbl.size(); i++) { 
+            int frequency_check = rounded_frequency;
+            if (to_string(frequency_check) == to_string(vbl[i])) { //check frequency is on the blacklist
               ison = true;
             }
           }
 
-          if (ison == false && new_frequency.bandwidth > config.minbandwidth) {
-            new_frequency.frequency = (rounded_frequency*1000); // add frequency to the list
-            new_frequency.timestamp = tool.gettimestamp();
-            frequencies.push_back(new_frequency);
+          if (ison == false && nfq.bandwidth > config.minbw) {
+            nfq.frequency = (rounded_frequency); // add frequency to the list
+            nfq.timestamp = gettimestamp();
+            vfq.push_back(nfq);
           }
         }
         pib++;
         i = pib;
-        if (config.verbous) tool.debug("go ahead on index " + to_string(i), false);
+        if (config.verbous) debug("go ahead on index " + to_string(i), false);
       }
-      tool.debug(to_string(frequencies.size()) + " frequencies saved in the list", false);
+      debug(to_string(vfq.size()) + " frequencies saved in the list", false);
       string out = "# created with dxlAPRS_scanner\n\n";
 
-      for (auto& element : frequencies) { // creating the frequencieslist for dxlAPRS
-        tool.debug(to_string(element.frequency) + " " + to_string(element.bandwidth) + " " + to_string(element.timestamp), false);
-        string out_frequency = to_string(element.frequency);
+      for (auto& vfge : vfq) { // creating the frequencieslist for dxlAPRS
+        debug(to_string(vfge.frequency) + " " + to_string(vfge.bandwidth) + " " + to_string(vfge.timestamp), false);
+        string out_frequency = to_string(vfge.frequency);
 
-        out.append("f " + out_frequency.insert(3, ".") + " 5 " + to_string(config.squelch) + " 0 " + to_string(element.bandwidth * 1000) + "\n");
+        out.append("f " + out_frequency.insert(3, ".") + " 5 " + to_string(config.squelch) + " 0 " + to_string(vfge.bandwidth * 1000) + "\n");
       }
-      for (i = 0; i < frequencies.size(); i++) { 
-        if ( (tool.gettimestamp() - frequencies[i].timestamp) > config.timer_holding) { //check timestamp and remove from list
-          frequencies.erase(frequencies.begin() + i);
+      for (i = 0; i < vfq.size(); i++) { 
+        if ( (gettimestamp() - vfq[i].timestamp) > config.timer_holding) { //check timestamp and remove from list
+          vfq.erase(vfq.begin() + i);
         }
       }
 
@@ -309,7 +297,7 @@ int getpeaks() {
 
       out.append("s " + out_startfrequency.insert(3, ".") + " " + out_stopfrequency.insert(3, ".") + " " + to_string(config.steps) + " 6 3000\n");
 
-      tool.file_write(config.filename, out);
+      file_write(config.filename, out);
 
       out = "";
     }
@@ -322,8 +310,8 @@ int getpeaks() {
 
 
 int main(int argc, char** argv) {
-  tool.debug("dxlAPRS_scanner - dxlAPRS extension for scanning frequencies", false);
-  tool.debug("Copyright (C) Jean-Michael Grobel (DO2JMG) <do2jmg@do2jmg.eu>", true);
+  debug("dxlAPRS_scanner - dxlAPRS extension for scanning frequencies", false);
+  debug("Copyright (C) Jean-Michael Grobel (DO2JMG) <do2jmg@do2jmg.eu>", true);
 
   sleep(3);
 
@@ -335,7 +323,7 @@ int main(int argc, char** argv) {
       if(i+1 < argc) {
         config.port = stoi(argv[i+1]);
       } else {
-        tool.debug("Error : Port", false);
+        debug("Error : Port", false);
         return 0;
       }
     } 
@@ -344,7 +332,7 @@ int main(int argc, char** argv) {
         config.steps = stoi(argv[i+1]);
         if (config.steps < 1500) {config.steps = 1500; } // 1500hz is minimum
       } else {
-        tool.debug("Error : Steps", false);
+        debug("Error : Steps", false);
         return 0;
       }
     } 
@@ -352,7 +340,7 @@ int main(int argc, char** argv) {
       if(i+1 < argc) {
         config.startfrequency = stoi(argv[i+1]); // start frequency in hz
       } else {
-        tool.debug("Error : Startfrequency", false);
+        debug("Error : Startfrequency", false);
         return 0;
       }
     } 
@@ -360,7 +348,7 @@ int main(int argc, char** argv) {
       if(i+1 < argc) {
         config.filename = argv[i+1];
       } else {
-        tool.debug("Error : Filename", false);
+        debug("Error : Filename", false);
         return 0;
       }
     } 
@@ -368,7 +356,7 @@ int main(int argc, char** argv) {
       if(i+1 < argc) {
         config.timer_holding = stoi(argv[i+1]);
       } else {
-        tool.debug("Error : Holding timer", false);
+        debug("Error : Holding timer", false);
         return 0;
       }
     } 
@@ -376,7 +364,7 @@ int main(int argc, char** argv) {
       if(i+1 < argc) {
         config.blacklist = argv[i+1];
       } else {
-        tool.debug("Error : Blacklist", false);
+        debug("Error : Blacklist", false);
         return 0;
       }
     } 
@@ -384,7 +372,7 @@ int main(int argc, char** argv) {
       if(i+1 < argc) {
         config.squelch = stoi(argv[i+1]);
       } else {
-        tool.debug("Error : Squelch", false);
+        debug("Error : Squelch", false);
         return 0;
       }
     } 
@@ -392,7 +380,7 @@ int main(int argc, char** argv) {
       if(i+1 < argc) {
         config.level = stoi(argv[i+1]);
       } else {
-        tool.debug("Error : Level", false);
+        debug("Error : Level", false);
         return 0;
       }
     } 
