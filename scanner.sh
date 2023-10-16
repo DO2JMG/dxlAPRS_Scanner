ee11322e3dc768cd7a47490d38a03643ccf65ede
Jean-Michael <34538443+DO2JMG@users.noreply.github.com>
2023-10-16 07:57:50 +0200

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -3,10 +3,8 @@
 workingdir="$( cd "$(dirname "$0")" ; pwd -P )"
 bindir=${workingdir}/bin
 
-
 scanner=${bindir}/scanner
 
 command -v ${scanner} >/dev/null 2>&1 || { echo "Ich vermisse " ${scanner} >&2; exit 1; }
 
-
-${scanner} -p 18051 -u 17051 -f 404000000 -s 1500 -v -o sdrcfg-rtl0.txt -b blacklist.txt -q 55 -n 5 &
+${scanner} -p 18051 -u 17051 -f 404000000 -s 1500 -v -o sdrcfg-rtl0.txt -b blacklist.txt -w whitelist.txt -q 55 -n 5 &
