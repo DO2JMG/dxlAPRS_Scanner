c74e6bf3c4cb15bc19d33668051ab03682b7ae8e
Jean-Michael <34538443+DO2JMG@users.noreply.github.com>
2023-10-18 11:02:10 +0200

    Add files via upload

--- src/tools.cpp
+++ src/tools.cpp
@@ -48,6 +48,16 @@ namespace tools {
     return t;
   }
 
+  string gettime() {
+    time_t     now = time(0);
+    struct tm  tstruct;
+    char       buf[80];
+    tstruct = *localtime(&now);
+
+    strftime(buf, sizeof(buf), "%Y-%m-%d %X", &tstruct);
+    return buf;
+  }
+
   vector<string> splitString(const string& str) {
       std::vector<std::string> tokens;
       try {
